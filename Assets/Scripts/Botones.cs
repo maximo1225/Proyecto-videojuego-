6d52c30d21bed49cad20f463eabbc8a5e3f44342
Maximo Rodriguez <maximo1225@gmail.com>
2016-09-25 15:51:43 -0400

    feat: clase para manejo de los botones
    Esta clase maneja el evento de cuando se selecciona un boton este
    muestre un valor.

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Botones : MonoBehaviour {
+
+    public Button boton;
+    public Text botonTexto;
+    private ControladorJ controladorJ;
+
+    public void SetSpace()
+    {
+        botonTexto.text = controladorJ.GetTurnoString();
+        boton.interactable = false;
+        controladorJ.FinalizarTurno();
+    }
+
+    public void SetGameControllerReference(ControladorJ controlador)
+    {
+        controladorJ = controlador;
+    }
+
+}
