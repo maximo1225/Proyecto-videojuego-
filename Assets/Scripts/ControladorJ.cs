bfc47f43e721859c280e5666e514665a2b0e653e
Maximo Rodriguez <maximo1225@gmail.com>
2016-09-25 16:40:39 -0400

    Style: punto y coma faltante

--- Assets/Scripts/ControladorJ.cs
+++ Assets/Scripts/ControladorJ.cs
@@ -81,7 +81,7 @@ public class ControladorJ : MonoBehaviour {
         if(movNum >= 9)
         {
             mensaje.SetActive(true);
-            TextoMensaje.text = "Empate!"
+            TextoMensaje.text = "Empate!";
         }
 
         Cambiardeturno();
